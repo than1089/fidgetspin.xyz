20d9fbdbc4d0f11d09521dbe86d0aaf745733209
Ngo Hoai Than <than1089@gmail.com>
2017-10-09 18:16:18 +0700

    update

--- withdraw.js
+++ withdraw.js
@@ -1,3 +1,5 @@
-localStorage.setItem('fidget_max_velocity', 0);
-localStorage.setItem('fidget_spins', 0);
-location.href = 'index.html';
+function  resetScore() {
+	localStorage.setItem('fidget_max_velocity', 0);
+	localStorage.setItem('fidget_spins', 0);
+	location.href = 'index.html';
+}
